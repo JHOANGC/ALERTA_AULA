b633c704b148870f77688d01e521b69521185d92
JHOANGC <andergonza621@gmail.com>
2026-08-12 23:11:14 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -24,7 +24,8 @@ messaging.onBackgroundMessage((payload) => {
     body: cuerpo,
     icon: "icon-192.png",
     vibrate: [250, 100, 250, 100, 250],
-    tag: "alerta-aula"
+    tag: "alerta-aula",
+    renotify: true
   });
 });
  
